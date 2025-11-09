f348182d3986dc00de2521bc26c1875ede0ad1fc
kk_666 <103745673+godragnar@users.noreply.github.com>
2025-11-09 20:05:08 +0530

    Update app.java

--- app.java
+++ app.java
@@ -3,3 +3,20 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+ //dev2 pushed from his local system to github
+    public class VariablesExample {
+    public static void main(String[] args) {
+        int age = 30; // Integer variable
+        String name = "Alice"; // String variable
+        double price = 19.99; // Double-precision floating-point variable
+        boolean isActive = true; // Boolean variable
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Is Active: " + isActive);
+    }
+}
+
+    
